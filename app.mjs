c42fc004f8e994831daa2c3ae6b6568f518f666d
Егор <legors1989@gmail.com>
2025-10-04 11:40:00 +0300

    Test commit

---
+++ app.mjs
@@ -0,0 +1,8 @@
+import log, { greet, persons } from "./characters.mjs";
+
+log();
+
+for (const c of persons) {
+  greet(c);
+}
+console.log(1);
---
+++ characters.mjs
@@ -0,0 +1,9 @@
+export const persons = ["1", "2"];
+
+export function greet(car) {
+  console.log(`Log ${car}`);
+}
+
+export default function log() {
+  console.log('log')
+}
